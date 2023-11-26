117f6bf87c65c7eafd556b2e0eee822847e2a690
Eyellen <83500523+Eyellen@users.noreply.github.com>
2023-11-26 20:52:46 +0300

    Added Simple.StateMachineBehaviour.cs.

---
+++ Runtime/Simple/StateMachineBehaviour.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+namespace Eyellen.Unity.StateMachines.Simple
+{
+    public abstract class StateMachineBehaviour<TStateMachine, TStateEnum, TBaseState> : MonoBehaviour
+        where TStateMachine : StateMachine<TStateEnum, TBaseState>, new()
+        where TStateEnum : Enum
+        where TBaseState : State<TStateEnum>
+    {
+        private TStateMachine _stateMachine;
+
+        protected virtual void Awake()
+        {
+            _stateMachine = new TStateMachine();
+        }
+
+        protected virtual void Start()
+        {
+
+        }
+
+        protected virtual void Update()
+        {
+            _stateMachine.Update();
+        }
+
+        protected virtual void LateUpdate()
+        {
+            _stateMachine.LateUpdate();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            _stateMachine.FixedUpdate();
+        }
+
+        protected virtual void OnCollisionEnter(Collision collision)
+        {
+            _stateMachine.OnCollisionEnter(collision);
+        }
+
+        protected virtual void OnCollisionExit(Collision collision)
+        {
+            _stateMachine.OnCollisionExit(collision);
+        }
+
+        protected virtual void OnCollisionStay(Collision collision)
+        {
+            _stateMachine.OnCollisionStay(collision);
+        }
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            _stateMachine.OnTriggerEnter(other);
+        }
+
+        protected virtual void OnTriggerExit(Collider other)
+        {
+            _stateMachine.OnTriggerExit(other);
+        }
+
+        protected virtual void OnTriggerStay(Collider other)
+        {
+            _stateMachine.OnTriggerStay(other);
+        }
+    }
+}
