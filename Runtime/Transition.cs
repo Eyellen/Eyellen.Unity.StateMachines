9bc54e7d2aa1fac6f7c900ef35406ccff77eea97
Eyellen <83500523+Eyellen@users.noreply.github.com>
2024-04-10 20:02:56 +0300

    Added additional condition for Transition constructor

--- Runtime/Transition.cs
+++ Runtime/Transition.cs
@@ -17,6 +17,10 @@ namespace Eyellen.Unity.StateMachines
                 if (to == null)
                     throw new NullReferenceException($"{nameof(to)} is null.");
 
+                if (to.StateMachine == null)
+                    throw new ArgumentException($"Given {nameof(to)} state is not located in any state machine.\n" +
+                        $"To create a transition to a state it must be in a state machine.");
+
                 StateMachine = to.StateMachine;
                 ToState = to;
                 _condition = condition;
